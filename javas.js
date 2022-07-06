beaedc3e2c7c515b53024d4fc9bd9397dee7a256
AdCh <adilchorfi10@gmail.com>
2022-07-06 10:37:06 +0100

    fixing the average profit and add total profit + some minor changes

--- javas.js
+++ javas.js
@@ -16,8 +16,14 @@ function submitting()
     if(returnmoney!=="" && invest!=="")
     {
   
+        let profitPercentage = 100*(returnmoney -invest)/invest
 
-        letmyArray = [timing,invest,returnmoney,returnmoney -invest]
+        if(profitPercentage - Math.floor(profitPercentage) !== 0)
+        {
+            profitPercentage=profitPercentage.toFixed(2)
+        }
+
+        letmyArray = [timing,invest,returnmoney,profitPercentage+"%"]
         let row= document.createElement("tr")
         dataTable.appendChild(row)
         for(let i=0;i<4;i++)
@@ -31,6 +37,7 @@ function submitting()
     }
     document.getElementById("invest").value = ""
     document.getElementById("return").value = ""
+
     updateData()
     
 
@@ -39,7 +46,7 @@ function submitting()
 
 function updateData()
 {
-    let totalInvest=0;totalReturn=0;avgProfit=0
+    let totalInvest=0;totalReturn=0;avgProfit=0;totalProfit=0;
     var tInvest = document.getElementsByClassName("class_1");
     var tReturn = document.getElementsByClassName("class_2");
     var tProfit = document.getElementsByClassName("class_3");
@@ -47,21 +54,21 @@ function updateData()
     {
     totalInvest=totalInvest + Number(tInvest[i].innerHTML)
     totalReturn=totalReturn + Number(tReturn[i].innerHTML)
-    avgProfit=avgProfit + Number(tProfit[i].innerHTML)
+    avgProfit=avgProfit + Number(tProfit[i].innerHTML.replace("%",""))
+    totalProfit = totalProfit + Number(tReturn[i].innerHTML)-Number(tInvest[i].innerHTML)
     }
     avgProfit=avgProfit/tProfit.length
     document.getElementById("tInvest").innerHTML="£"+totalInvest
     document.getElementById("tReturn").innerHTML="£"+totalReturn
-    let avg = 100*avgProfit/totalInvest
-    if(avg - Math.floor(avg) !== 0)
+    if(avgProfit - Math.floor(avgProfit) !== 0)
     {
-        document.getElementById("avgProfit").innerHTML=avg.toFixed(2)+"%"
+        document.getElementById("avgProfit").innerHTML=avgProfit.toFixed(2)+"%"
     }
     else
     {
-    document.getElementById("avgProfit").innerHTML=avg+"%"
+    document.getElementById("avgProfit").innerHTML=avgProfit+"%"
     }
-    if(avg <0)
+    if(avgProfit <0)
     {
         document.getElementById("avgProfit").style.color="red"
     }
@@ -69,5 +76,16 @@ function updateData()
     {
         document.getElementById("avgProfit").style.color="green"
     }
+
+    document.getElementById("totalProfit").innerHTML="£"+totalProfit
+    if(totalProfit>0)
+    {
+        document.getElementById("totalProfit").style.color="green"
+    }
+    else if(totalProfit<0)
+    {
+        document.getElementById("totalProfit").style.color="red"
+    }
+        
 
 }
